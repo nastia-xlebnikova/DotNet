be48b4aad42f7a74427fe12b1f226bda59a79bd2
nastia-xlebnikova <78591002+nastia-xlebnikova@users.noreply.github.com>
2021-12-16 17:44:55 +0200

    add lab7

---
+++ lab7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace khlebnikova7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var studList = new Container();
+            studList.Add(new Student("Lugovoy Alexander Evgenievich", new DateTime(2000, 10, 8), new DateTime(2019, 9, 1), "b", 119, "CIT", "Computer engineering", 96));
+            studList.Add(new Student("Babich Dmitry Olegovich", new DateTime(1998, 10, 10), new DateTime(2017, 9, 1), "b", 217, "E", "Power engineering", 68));
+            studList.Add(new Student("Orlova Vlada Stanislavovna", new DateTime(2001, 12, 8), new DateTime(2019, 9, 1), "b", 119, "CIT", "Computer engineering", 94));
+            studList.Add(new Student("Khlebnikova Anastasia Dmitrievna", new DateTime(2002, 12, 10), new DateTime(2019, 9, 1), "b", 119, "CIT", "Computer engineering", 86));
+
+            Console.WriteLine();
+            Console.WriteLine(ContTable.ToTable(studList, Student.CompareSpecialty, "Computer engineering"));
+            studList.Reset();
+            Console.WriteLine("Средний возраст группы КИТ-119Б: " + ContCalc.AverageAge(studList, Student.CompareGroup, "CIT-119b"));
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(studList, options);
+
+            Console.WriteLine(jsonString);
+
+            Container studList2 = new Container(JsonSerializer.Deserialize<List<Student>>(jsonString));
+            foreach (Student stud in studList2)
+            {
+                Console.WriteLine(stud.ToText());
+            }
+        }
+    }
+}
